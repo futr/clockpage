c0653ac4789cef7ad98e1e5d63b8d969476dbc05
Masato Takahashi <ilce.ma@gmail.com>
2021-04-27 11:56:52 +0900

    Add clear button

--- js.js
+++ js.js
@@ -6,23 +6,40 @@ function updateClock()
     let nowStr = dateFns.format(time, 'HH:mm:ss', {locale: dateFns.ja});
 
     let currentElem = document.getElementById( "currentTime" );
+    let countUpElem = document.getElementById( "countUp" );
 
     currentElem.innerText = nowStr;
     
     if ( startTime != null ) {
-        let countUpElem = document.getElementById( "countUp" );
-        let countUpSec = dateFns.differenceInSeconds( time, startTime );
-        let countUpStr = new Date( countUpSec * 1000 ).toISOString().substr( 11, 8 );
-        countUpElem.innerText = countUpStr;
+        let sec = dateFns.differenceInSeconds( time, startTime );
+        
+        let h = Math.floor( sec / 3600 );
+        let m = Math.floor( ( sec % 3600 ) / 60 );
+        let s = sec % 60;
+        
+        h = ( '0' + h ).slice( -2 );
+        m = ( '0' + m ).slice( -2 );
+        s = ( '0' + s ).slice( -2 );
+        
+        countUpStr = h + ":" + m + ":" + s;
+        
+        countUpElem.textContent = countUpStr;
+    } else {
+        countUpElem.textContent = "";
     }
 }
 
 window.addEventListener( "DOMContentLoaded", () => {
     document.getElementById( "startButton" ).onclick = e => {
         startTime = new Date();
-        
+        updateClock();
+    };
+    
+    document.getElementById( "clearButton" ).onclick = e => {
+        startTime = undefined;
+        updateClock();
     };
 
-    setInterval( updateClock, 500 );
+    setInterval( updateClock, 1000 );
 });
 
